646d9b646c0de850737f128565e2356beda5c91c
Max Wang <maxwellswang@gmail.com>
2018-05-17 22:00:32 -0400

    added heroku JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "node_mysql_table"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node_mysql_table"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
