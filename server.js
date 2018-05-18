7c6210c64ce9937f6fcf899fe78c990cb5c1a178
Max Wang <maxwellswang@gmail.com>
2018-05-17 22:11:12 -0400

    working on heroku deployment

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var port = process.env.PORT || 3000;
-
+var exphbs = require("express-handlebars");
 var app = express();
+
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -21,4 +18,6 @@ var routes = require("./routes/htmlRoutes.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT, function() {
+    console.log("App listening on port: " + PORT);
+});
